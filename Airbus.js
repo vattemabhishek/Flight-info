dbf383c5d6a4b7f678e08ae7371af61ab8efa45b
vattemabhishek <vattemabhishek@gmail.com>
2023-08-26 15:24:16 +0530

    initial commit

---
+++ Airbus.js
@@ -0,0 +1,17 @@
+const  flightInfo1 = [{
+    name: "A320",
+    description:"The Airbus A320 family is a series of narrow-body airliners developed and produced by Airbus.",
+    image:"https://en.wikipedia.org/wiki/Airbus_A320_family#/media/File:Jetstar_Airbus_A320_in_flight_(6768081241)_crop.jpg"
+},
+{
+    name: "A330",
+    description:"The Airbus A330 is a wide-body aircraft developed and produced by Airbus.",
+    image:"https://en.wikipedia.org/wiki/Airbus_A330#/media/File:Turkish_Airlines,_Airbus_A330-300_TC-JNL_NRT_(23708073592).jpg"
+},
+{
+    name: "A380",
+    description:"The Airbus A380 is a large wide-body airliner that was developed and produced by Airbus.",
+    image:"https://en.wikipedia.org/wiki/Airbus_A380#/media/File:A6-EDY_A380_Emirates_31_jan_2013_jfk_(8442269364)_(cropped).jpg"
+}
+]
+
---
+++ practics.js
@@ -0,0 +1,5 @@
+function add (a,b){
+const sum = a+b;
+return sum;
+}
+console.log(add (5,10));
